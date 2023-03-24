31ea9d5f6e92c5db35dbb588fa3ac5e471e6209b
soraamii <thfkazoa@gmail.com>
2023-03-24 09:36:13 +0900

    chapter
    chapter

---
+++ javaproject/FirstJava/src/chapter/Member.java
@@ -0,0 +1,40 @@
+package chapter;
+
+public class Member {
+	
+	
+
+	public static void main(String[] args) {
+	
+	// 1-2
+	System.out.println("이소람");
+		
+	// 1-3
+	int a = 1;
+	int b = 2;
+	
+	
+	
+	System.out.println(a+b);
+	System.out.println(a-b);
+	System.out.println(a*b);
+	System.out.println(a/b);
+	
+	// 2-1
+	String name = "이소람";
+	int age = 15;
+	double height = 180.5;
+	boolean hasBook = true;
+	
+	System.out.println(name);
+	System.out.println(age);
+	System.out.println(height);
+	System.out.println(hasBook);
+	
+	
+	
+	
+
+	}
+
+}
---
+++ javaproject/FirstJava/src/chapter/Person.java
@@ -0,0 +1,16 @@
+package chapter;
+
+public class Person {
+
+	public static void main(String[] args) {
+		
+		String name = "이소람";
+		String phone = "010-2406-4924";
+		String id = "930419-1234567";
+		
+		
+	
+
+	}
+
+}
