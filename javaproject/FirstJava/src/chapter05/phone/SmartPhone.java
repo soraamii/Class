3150f4a2e965e76a3f8ec3812ef6a7be8e2d1cad
soraamii <thfkazoa@gmail.com>
2023-03-27 18:21:53 +0900

    chapter05
    chapter05

---
+++ javaproject/FirstJava/src/chapter05/phone/SmartPhone.java
@@ -0,0 +1,58 @@
+package chapter05.phone;
+
+public class SmartPhone {
+	
+	// 인스턴스 변수
+	String color;
+	float size;
+	int volume;
+	
+	// 생성자 : 초기화 메서드, 인스턴스 변수의 초기화 작업
+	// 초기화 작업이 없는 경우 생략 가능
+	// 아래와 같은 생성자 = 기본 생성자
+//	SmartPhone(){
+//		System.out.println("인스턴스 생성!");
+//	}
+	
+	// 초기화를 위한 생성자 정의
+	SmartPhone(String color, float size, int volume){
+		this.color = color;
+		this.size = size;
+		this.volume = volume;
+		
+	}
+	
+	
+	SmartPhone(String color, float size){
+		
+		this(color, size, 0); // this() -> 다른 생성자 호출
+		
+//		this.color = color;
+//		this.size = size;
+//		this.volume = 0;
+		
+	}
+	
+	SmartPhone(){
+		
+		this("white", 4.5f, 5);
+		
+//		color = "WHITE";
+//		size = 4.5f;
+//		volume = 5;
+	}
+	
+	
+	void call() {
+		System.out.println("전화 걸기!");
+	}
+	
+	void volumeUP() {
+		volume++;
+	}
+	
+	void volumeDown() {
+		volume--;
+	}
+
+}
---
+++ javaproject/FirstJava/src/chapter05/phone/SmartPhoneMain.java
@@ -0,0 +1,29 @@
+package chapter05.phone;
+
+public class SmartPhoneMain {
+
+	public static void main(String[] args) {
+		
+		SmartPhone sp1 = new SmartPhone("RED", 5.0f, 10);
+		SmartPhone sp2 = new SmartPhone("Gold",4.0f);
+		SmartPhone sp3 = new SmartPhone();
+		// 순서 1. new SmartPhone 
+		// 순서 2. SmartPhone("RED", 5.0f, 10)
+		
+//		sp.color = "WHITE";
+//		sp.size = 4.5f;
+//		sp.volume = 5;
+	
+				
+		System.out.println(sp1.color);
+		System.out.println(sp1.size);
+		System.out.println(sp1.volume);
+		System.out.println(sp2.color);
+		System.out.println(sp2.size);
+		System.out.println(sp2.volume);
+		System.out.println(sp3.color);
+		System.out.println(sp3.size);
+		System.out.println(sp3.volume);
+	}
+	
+}
