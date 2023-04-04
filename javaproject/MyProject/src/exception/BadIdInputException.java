cfb3b9b155f379d59f7fb9c2639463916981d22d
soraamii <thfkazoa@gmail.com>
2023-04-04 16:41:17 +0900

    re chp10
    re chp10

--- javaproject/MyProject/src/exception/BadIdInputException.java
+++ javaproject/MyProject/src/exception/BadIdInputException.java
@@ -2,12 +2,10 @@ package exception;
 
 public class BadIdInputException extends Exception {
 
-	//② 예외 클래스 이름은 BadIdInputException이라고 정의합시다
+	// ② 예외 클래스 이름은 BadIdInputException이라고 정의합시다
 	public BadIdInputException(String message) {
 		super("사용자의 입력 값이 올바르지 않습니다.");
-		
+
 	}
-	
-	
 
 }
--- javaproject/MyProject/src/exception/ExceptionExam10.java
+++ javaproject/MyProject/src/exception/ExceptionExam10.java
@@ -5,37 +5,32 @@ import java.util.Scanner;
 public class ExceptionExam10 {
 
 	public static void main(String[] args) {
-		//콘솔에서 사용자 아이디를 입력 받아 
-		//정상적인 영문자와 숫자로만 이루어진 값을 입력했는지 확인하는 프로그램을 만들어봅시다.
+		// 콘솔에서 사용자 아이디를 입력 받아
+		// 정상적인 영문자와 숫자로만 이루어진 값을 입력했는지 확인하는 프로그램을 만들어봅시다.
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("영문자와 숫자로 이루어진 값을 입력하시오.");
-		
+
 		String id = null;
-		
-		 
-		//① 사용자 예외 클래스를 정의해서 예외를 발생 시켜 봅시다.
-		
-		
-			id = sc.nextLine();
-			for(int i = 0; i < id.length(); i++) {
-				char msg = id.charAt(i);
-			
-		try {
-			
-			if(!(msg >= 'a' && msg <= 'z' || msg >= 'A' && msg <= 'Z')) {
-				BadIdInputException e = new BadIdInputException("잘못된 값 입력");
-				throw e;
-			}
-			} catch(BadIdInputException e) {
+
+		// ① 사용자 예외 클래스를 정의해서 예외를 발생 시켜 봅시다.
+
+		id = sc.nextLine();
+		for (int i = 0; i < id.length(); i++) {
+			char msg = id.charAt(i);
+
+			try {
+
+				if (!(msg >= 'a' && msg <= 'z' || msg >= 'A' && msg <= 'Z')) {
+					BadIdInputException e = new BadIdInputException("잘못된 값 입력");
+					throw e;
+				}
+			} catch (BadIdInputException e) {
 				e.printStackTrace();
 				System.out.println(e.getMessage());
-			
-		 
-			
-		}
-	
 
 			}
+
+		}
 	}
 }
